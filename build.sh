9db95b67d63cf80d13031bb2b053db7c664546cd
meets2tarun <meets2tarun@gmail.com>
2017-05-28 17:12:20 +0530

    Added image-i386

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ set -eu				# Be strict
 
 # Script parameters: arch mirror gnomelanguage release
 # Arch to build ISO for, i386 or amd64
-arch=${1:-amd64}
+arch=${1:-i386}
 # Ubuntu mirror to use
 mirror=${2:-"http://archive.ubuntu.com/ubuntu/"}
 # Set of GNOME language packs to install.
